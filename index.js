4f06ec2ba4b20f2da73e075314b188c25e703c95
Kevin McIntosh <kmac303@gmail.com>
2022-06-12 17:26:27 -0600

    commit tree ff37d7429dd24146cc453d5512594831963eebfb

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet(feet) {
+    distanceFromHqInBlocks(feet);
+    return Math.abs(feet - 42) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((destination - start) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+    let feet = Math.abs((destination - start) * 264);
+    if (feet <= 400) {
+        return 0;
+    }
+    else if (feet > 400 && feet <= 2000) {
+        return (feet - 400) * .02; 
+    }
+    else if (feet > 2000 && feet < 2500) {
+        return 25;
+    }
+    else if (feet > 2500) {
+        return 'cannot travel that far';
+    }
+}
